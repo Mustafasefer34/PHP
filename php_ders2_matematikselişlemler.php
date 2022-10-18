c18e1a84cdfb1c34438d2fc07f19db05a6d1bd42
Mustafa sefer <101596840+Mustafasefer34@users.noreply.github.com>
2022-10-18 16:21:36 +0300

    Add files via upload

---
+++ php_ders2_matematikselişlemler.php
@@ -0,0 +1,43 @@
+<?php
+   $kelime = "Sağlam";
+   $sayi1 = 10;
+   $sayi2 = 20;
+   
+
+
+   echo "Kelimenin veri tipi :".gettype($kelime);
+   echo "Sayının Veri tipi: ".gettype($sayi1);
+
+  /* $toplam = $sayi2 + $sayi1;
+   $fark = $sayi2 - $sayi1;
+   $bolum = $sayi2 / $sayi1;
+   $carpim = $sayi2 * $sayi1;
+   $kalan = $sayi2 % $sayi1;
+
+   echo("$toplam $fark $bolum $carpim $kalan");
+     
+
+     $sayi1 = $sayi1 +10;
+     $sayi2 += 10;
+
+     $sayi1 -=5;
+     $sayi2 -= 5;
+
+     $sayi1/=3;
+     $sayi2/=5;
+
+     $sayi1 *=2;
+     $sayi2 *=2;
+
+
+
+     echo("Yeni değer : $sayi1 <br> Yeni değer : $sayi2");
+
+     $sayi3=16;
+     echo sqrt($sayi3);
+     echo ("Sayının karekökü :".sqrt($sayi3));
+     echo ("Sayının üsttü : ".pow(4,2)."<br>");
+     echo ("Sayının mutlak değeri : ".abs(-3)."<br>");
+     echo ("En küçük sayı : ".min(4,2,5)."<br>");
+     echo ("En büyük sayı : ".max(4,2,10)."<br>");
+ ?>*/
