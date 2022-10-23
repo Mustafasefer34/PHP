99d47c20f07813e2dcf6c152aa3865de33fd6465
Mustafa sefer <101596840+Mustafasefer34@users.noreply.github.com>
2022-10-23 16:56:46 +0300

    Add files via upload

---
+++ Metinselİslemler.php
@@ -0,0 +1,14 @@
+<?php
+     
+     //$ad = "Mustafa Sefer";
+    // $soyad ="Kırkdokuz" ;
+     //echo "Adınız : ".$ad." <br> Soyadınız : ".$soyad;  
+      $sayı1 = 10;
+      $sayı2 = 20;
+      $mesaj ="Sayıların toplamı";
+      $toplam = $sayı2 + $sayı1;
+      //echo "Toplamı yaz : $toplam";
+        echo $mesaj." ".$toplam."<br>";
+        echo "Mesaj veri tipi : ".gettype($mesaj)."<br>";
+        echo "Toplam veri tipi : ".gettype($toplam)."<br>";
+?>
